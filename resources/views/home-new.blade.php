574496be94889539990b5a5651797dbc7447d388
roshanwebee <roshan.jajoriya@webeesocial.com>
2022-09-21 11:05:15 +0530

    google font change

--- resources/views/home-new.blade.php
+++ resources/views/home-new.blade.php
@@ -42,7 +42,7 @@
     <link rel="stylesheet" href="{{ asset('') }}/frontend/css/home.css">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body class="page-loaded color-undefined">
     <!-- Header -->
